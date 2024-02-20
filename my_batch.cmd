fe457ae1906d1ffe243dc763db7873ed97b67f63
Doug Kim <doug2026.kim@gmail.com>
2024-02-20 17:02:54 +0900

    Batch programming @office_24/02/20

--- my_batch.cmd
+++ my_batch.cmd
@@ -1,8 +1,8 @@
+@REM Studying batch programming
 
-::FOR 구문은 각 행별로 분석하는데 
-::이때 token은 각 행의 몇 번째 문자열을 전달해줄지 지정함.
-::문자열의 기본 구분단위는 공백임.
 @echo off
+
+goto todayStudy
 
 for /f "tokens=3" %%a in ('type test.log') do (
 	echo %%a
@@ -46,3 +46,48 @@ for /f "delims=: tokens=1,2" %%a in ("hello:world") do echo %%a-%%b
 
 for /f "skip=15 delims='#' tokens=1" %%a in (c:\Windows\System32\drivers\etc\hosts) do echo %%a
 for /f "skip=15 delims= tokens=1" %%a in (c:\Windows\System32\drivers\etc\hosts) do echo %%a
+
+@REM Commenting multiple lines in DOS batch file
+@REM Ctrl-Q
+
+:todayStudy
+
+::echo.This is batch programming sample.
+
+::Goto the location in the script
+pushd %~dp0
+
+echo.LCD Firmware update tool 2024/02/20
+echo.
+for /f "tokens=2 delims=\" %%a in ('wmic desktopmonitor get PNPDeviceID -value') do (
+	set PnPID=%%a
+)
+echo.
+echo.PnPID:%PnPID%
+
+if %PnPID% == LGD0755 goto LCD16_Z90R_VRR
+if %PnPID% == LGD0756 goto LCD17_Z90R_VRR
+goto failToFinePanel
+
+:LCD16_Z90R_VRR
+goto END
+
+:LCD16_Z90R_VRR
+echo.LCD 16 Model (VRR)
+:: ...
+:: ...
+:: ...
+goto END
+
+:LCD17_Z90R_VRR
+echo.LCD 17 Model (VRR)
+:: ...
+:: ...
+:: ...
+goto END
+
+:failToFinePanel
+echo *** ERROR : No proper LCD found ****
+
+
+:END
