4f6ead7f429d419544cb857919bd13775eac30d7
Doug Kim <doug.kim@lge.com>
2024-03-30 12:32:41 +0900

    doug's batch programming on 3/30th-1 @home

--- my_batch.cmd
+++ my_batch.cmd
@@ -4,22 +4,22 @@
 
 goto todayStudy
 
-:: ���ڿ��� ���(���ڿ��� Space�� ���е�)
+:: ���ڿ��� ���(���ڿ��� Space�� ���е�)
 for %%c in (miku daisuki) do (
 	echo %%c
 )
 
-:: ���� Directory�� ���� ��� ���
+:: ���� Directory�� ���� ��� ���
 for %%d in (*) do (
 	echo %%d
 )
 
-:: ���� Directory�� ���� ��ϰ� String ���
+:: ���� Directory�� ���� ��ϰ� String ���
 for %%d in (* miku daisuki) do (
 	echo %%d
 )
 
-:: Ư���� �ɼ�(Ư�� ����, Space�� ���е�)
+:: Ư���� �ɼ�(Ư�� ����, Space�� ���е�)
 for %%d in (1;2 3,4) do (
 	echo %%d
 )
@@ -33,15 +33,15 @@ pause
 :: /d (/D) : Directory for
 for /d %%a in (*) do echo %%a
 
-:: ���� For - ���۰� ��(Start,Delta,End)
+:: ���� For - ���۰� ��(Start,Delta,End)
 for /L %%a in (1,3,100) do echo %%a
 
 :: /r (/R) : Recursive for
 for /r d:\ %%a in (*.tx0, *.mp4) do echo %%a
 
 :: /f (/F) : File for
-:: tokens : �����ڷ� �������� ���� ���� ��ū�̶� �Ѵ�.
-:: delims(delimeters) : ���ڿ��� ������ ��ȣ ����, ������ ����(�����̽�, ��)���� ������.
+:: tokens : �����ڷ� �������� ���� ���� ��ū�̶� �Ѵ�.
+:: delims(delimeters) : ���ڿ��� ������ ��ȣ ����, ������ ����(�����̽�, ��)���� ������.
 :: skip :
 :: eol :
 :: usebackq :
@@ -58,9 +58,9 @@ for /f "delims=: tokens=1,2,3" %%a in ("hello:world!:this is doug.") do echo %%a
 
 type c:\Windows\System32\drivers\etc\hosts
 pause
-:: 15 lines�� skip�ϰ� #�� ���е� ù��° String�� ǥ���϶�.
+:: 15 lines�� skip�ϰ� #�� ���е� ù��° String�� ǥ���϶�.
 for /f "skip=15 delims='#' tokens=1" %%a in (c:\Windows\System32\drivers\etc\hosts) do echo %%a
-:: 15 lines�� skip�ϰ� �����ڰ� �����Ƿ� �� ���� ��ä�� ��ū�� �Ǿ %%a�� �����ȴ�.
+:: 15 lines�� skip�ϰ� �����ڰ� �����Ƿ� �� ���� ��ä�� ��ū�� �Ǿ %%a�� �����ȴ�.
 for /f "skip=15 delims= tokens=1" %%a in (c:\Windows\System32\drivers\etc\hosts) do echo %%a
 
 @REM Commenting multiple lines in DOS batch file
@@ -99,16 +99,37 @@ echo %E%
 echo %F%
 echo %G%
 
-@echo on
+@echo off
+echo 현재 작업 디렉토리가 실행 bat파일 경로와 다른 경우 확인
+cd
+echo bat파일의 전체 경로가 저장된 환경변수
+echo %0
+echo bat파일의 전체 경로에서 "(쌍따옴표)을 제거
+echo %~0
+echo bat파일의 전체 경로에서 드라이브 이름만 취득 
+echo %~d0
+echo bat파일의 전체 경로에서 드라이브 이름을 제외한 폴더 경로 만 취득 
+echo %~p0
+echo bat파일의 전체 경로에서 드라이브 이름을 포함한 폴더 경로 취득 
+echo %~dp0
+echo 현재 작업 디렉토리를 변경
+cd %~dp0
+echo 현재 작업 디렉토리 변경 결과 확인
+cd
+pause
 
+::@echo on
+echo batch 파일의 전체 경로에서 드라이브 이름을 포함한 폴더 경로 취득
 pushd %~dp0
 cd ..\..
+echo 드라이브를 포함한 현재 경로
 cd
+echo push한 드라이브를 포함한 전체 경로를 pop
 popd
 cd
 pause
 
-@echo off
+::@echo off
 
 goto END
 
