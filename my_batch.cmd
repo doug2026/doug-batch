c8b28822ebf0148d38d654035aee9665ce8d7793
Doug Kim <doug2026.kim@gmail.com>
2024-04-01 09:00:58 +0900

    Batch programming update on 4/1 3rd @Office

--- my_batch.cmd
+++ my_batch.cmd
@@ -4,22 +4,22 @@
 
 goto todayStudy
 
-:: ���ڿ��� ���(���ڿ��� Space�� ���е�)
+:: ���ڿ��� ���(���ڿ��� Space�� ���е�)
 for %%c in (miku daisuki) do (
 	echo %%c
 )
 
-:: ���� Directory�� ���� ��� ���
+:: ���� Directory�� ���� ��� ���
 for %%d in (*) do (
 	echo %%d
 )
 
-:: ���� Directory�� ���� ��ϰ� String ���
+:: ���� Directory�� ���� ��ϰ� String ���
 for %%d in (* miku daisuki) do (
 	echo %%d
 )
 
-:: Ư���� �ɼ�(Ư�� ����, Space�� ���е�)
+:: Ư���� �ɼ�(Ư�� ����, Space�� ���е�)
 for %%d in (1;2 3,4) do (
 	echo %%d
 )
@@ -33,15 +33,15 @@ pause
 :: /d (/D) : Directory for
 for /d %%a in (*) do echo %%a
 
-:: ���� For - ���۰� ��(Start,Delta,End)
+:: ���� For - ���۰� ��(Start,Delta,End)
 for /L %%a in (1,3,100) do echo %%a
 
 :: /r (/R) : Recursive for
 for /r d:\ %%a in (*.tx0, *.mp4) do echo %%a
 
 :: /f (/F) : File for
-:: tokens : �����ڷ� �������� ���� ���� ��ū�̶� �Ѵ�.
-:: delims(delimeters) : ���ڿ��� ������ ��ȣ ����, ������ ����(�����̽�, ��)���� ������.
+:: tokens : �����ڷ� �������� ���� ���� ��ū�̶� �Ѵ�.
+:: delims(delimeters) : ���ڿ��� ������ ��ȣ ����, ������ ����(�����̽�, ��)���� ������.
 :: skip :
 :: eol :
 :: usebackq :
@@ -58,13 +58,26 @@ for /f "delims=: tokens=1,2,3" %%a in ("hello:world!:this is doug.") do echo %%a
 
 type c:\Windows\System32\drivers\etc\hosts
 pause
-:: 15 lines�� skip�ϰ� #�� ���е� ù��° String�� ǥ���϶�.
+:: 15 lines�� skip�ϰ� #�� ���е� ù��° String�� ǥ���϶�.
 for /f "skip=15 delims='#' tokens=1" %%a in (c:\Windows\System32\drivers\etc\hosts) do echo %%a
-:: 15 lines�� skip�ϰ� �����ڰ� �����Ƿ� �� ���� ��ä�� ��ū�� �Ǿ %%a�� �����ȴ�.
+:: 15 lines�� skip�ϰ� �����ڰ� �����Ƿ� �� ���� ��ä�� ��ū�� �Ǿ %%a�� �����ȴ�.
 for /f "skip=15 delims= tokens=1" %%a in (c:\Windows\System32\drivers\etc\hosts) do echo %%a
 
 @REM Commenting multiple lines in DOS batch file
 @REM Ctrl-Q
+
+for /f "tokens=6 delims== " %%i in ('"wmic cpu get Caption /value | findstr /b /c:"Caption""') do set CPU_GEN=%%i
+  wmic cpu get Caption /value
+  IF [%CPU_GEN%]==[141] SET CPU_NAME=TGL
+  IF [%CPU_GEN%]==[140] SET CPU_NAME=TGL
+  IF [%CPU_GEN%]==[154] SET CPU_NAME=ADL
+  IF [%CPU_GEN%]==[156] SET CPU_NAME=JSL
+  IF [%CPU_GEN%]==[186] SET CPU_NAME=RPL
+  IF [%CPU_GEN%]==[170] SET CPU_NAME=MTL
+@echo %CPU_GEN%
+@echo %CPU_NAME%
+
+goto END
 
 
 :todayStudy
@@ -89,7 +102,8 @@ SET C=%~p0
 SET D=%~n0
 SET E=%~x0
 SET F=%~dp0
-SET G=%~d0\%~p0
+SET G=%~dpnx0
+SET H=%~d0\%~p0
 
 echo %A%
 echo %B%
@@ -98,36 +112,35 @@ echo %D%
 echo %E%
 echo %F%
 echo %G%
+echo %H%
+
+::set TESTDIR=%~dp0%..
+set TESTDIR=%~dp0%test_dir
+@echo %TESTDIR%
+::cd %TESTDIR%
+::@call test.bat
+@call "%~dp0test.bat"
 
 @echo off
-echo 현재 작업 디렉토리가 실행 bat파일 경로와 다른 경우 확인
 cd
-echo bat파일의 전체 경로가 저장된 환경변수
 echo %0
-echo bat파일의 전체 경로에서 "(쌍따옴표)을 제거
 echo %~0
-echo bat파일의 전체 경로에서 드라이브 이름만 취득 
 echo %~d0
-echo bat파일의 전체 경로에서 드라이브 이름을 제외한 폴더 경로 만 취득 
 echo %~p0
-echo bat파일의 전체 경로에서 드라이브 이름을 포함한 폴더 경로 취득 
 echo %~dp0
-echo 현재 작업 디렉토리를 변경
 cd %~dp0
-echo 현재 작업 디렉토리 변경 결과 확인
 cd
+@echo off
 pause
 
-::@echo on
-echo batch 파일의 전체 경로에서 드라이브 이름을 포함한 폴더 경로 취득
+@echo on
 pushd %~dp0
 cd ..\..
-echo 드라이브를 포함한 현재 경로
 cd
-echo push한 드라이브를 포함한 전체 경로를 pop
 popd
 cd
 pause
+@echo off
 
 ::@echo off
 
